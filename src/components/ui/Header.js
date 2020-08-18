5fcc3cc2918149824abae41eb53470076c7c9f3f
sebv <sebv@sv.home>
2020-08-18 15:10:53 +0200

    first commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='center'>
+        <img src={logo} alt='' />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
